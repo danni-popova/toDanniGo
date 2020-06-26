bc3917488a753454ab5e3718ee93c29bbfe2f358
Danni Popova <danni.popova@unity3d.com>
2020-06-26 17:20:53 +0100

    refactor: deleting some stuff that's not needed anymore

--- services/todo/server.go
+++ services/todo/server.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -21,23 +22,13 @@ const (
 	dbname   = "todo"
 )
 
-type ToDoRequest struct {
+type ToDo struct {
 	UserID      int       `json:"user_id,omitempty" db:"user_id"`
 	ID          int       `json:"id,omitempty" db:"todo_id"`
 	Title       string    `json:"title, omitempty" db:"title"`
 	Description string    `json:"description,omitempty" db:"description"`
 	Deadline    time.Time `json:"deadline,omitempty" db:"deadline"`
 	CreatedAt   time.Time `json:"created_at,omitempty" db:"created_at"`
-	Done        bool      `json:"done" db:"done"`
-}
-
-type ToDo struct {
-	ID          int       `json:"id" db:"todo_id"`
-	UserID      int       `json:"user_id" db:"user_id"`
-	Title       string    `json:"title" db:"title"`
-	Description string    `json:"description,omitempty" db:"description"`
-	CreatedAt   time.Time `json:"created_at" db:"created_at"`
-	Deadline    time.Time `json:"deadline,omitempty" db:"deadline"`
 	Done        bool      `json:"done" db:"done"`
 }
 
@@ -74,9 +65,31 @@ func getTodos(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(marshalled))
 }
 
+func getTodo(w http.ResponseWriter, r *http.Request) {
+	//TODO: Authenticate
+	pathParams := mux.Vars(r)
+	rID := pathParams["todo_id"]
+
+	// Retrieve from database
+	db := dbCon()
+	var td []ToDo
+	sqlQuery := fmt.Sprintf("SELECT * FROM todo WHERE todo_id=%s", rID)
+	err := db.Select(&td, sqlQuery)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// Write response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// TODO: Check for error when marshaling
+	marshalled, err := json.Marshal(td[0])
+	w.Write([]byte(marshalled))
+}
+
 func createTodo(w http.ResponseWriter, r *http.Request) {
 	reqBody, _ := ioutil.ReadAll(r.Body)
-	var td ToDoRequest
+	var td ToDo
 	err := json.Unmarshal(reqBody, &td)
 	if err != nil {
 		fmt.Println(err)
@@ -105,44 +118,49 @@ func createTodo(w http.ResponseWriter, r *http.Request) {
 
 func updateTodo(w http.ResponseWriter, r *http.Request) {
 	reqBody, _ := ioutil.ReadAll(r.Body)
-	var toDoRequest ToDoRequest
+	var toDoRequest ToDo
 	err := json.Unmarshal(reqBody, &toDoRequest)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	// Get the id from the request
 	pathParams := mux.Vars(r)
 	rID := pathParams["todo_id"]
 
-	var oldTd ToDoRequest
+	var statements []string
+	var params []interface{}
+	var baseQuery = "UPDATE todo SET %v WHERE todo_id=$%d"
+
+	if toDoRequest.Title != "" {
+		statements = append(statements, fmt.Sprintf("title=$%d", len(statements)+1))
+		params = append(params, toDoRequest.Title)
+	}
+	if toDoRequest.Description != "" {
+		statements = append(statements, fmt.Sprintf("description=$%d", len(statements)+1))
+		params = append(params, toDoRequest.Description)
+	}
+	if !toDoRequest.Deadline.IsZero() {
+		statements = append(statements, fmt.Sprintf("deadline=$%d", len(statements)+1))
+		params = append(params, toDoRequest.Deadline)
+	}
+	if toDoRequest.Done {
+		statements = append(statements, fmt.Sprintf("done=$%d", len(statements)+1))
+		params = append(params, true)
+	}
+
+	params = append(params, rID)
+	updateStatements := strings.Join(statements, ", ")
+	querySql := fmt.Sprintf(baseQuery, updateStatements, len(statements)+1)
+
 	db := dbCon()
-	err = db.QueryRowx("SELECT * FROM todo WHERE todo_id=$1", rID).StructScan(&oldTd)
+	result, err := db.Query(querySql, params...)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	if toDoRequest.Title != "" {
-		oldTd.Title = toDoRequest.Title
-	}
-	if toDoRequest.Description != "" {
-		oldTd.Description = toDoRequest.Description
-	}
-	if !toDoRequest.Deadline.IsZero() {
-		oldTd.Deadline = toDoRequest.Deadline
-	}
-	if toDoRequest.Done {
-		oldTd.Done = true
-	}
-
-	// Write to database
-	_, err = db.NamedQuery(`UPDATE todo 
-								  SET title=:title, 
-                				  description=:description, 
-                				  deadline=:deadline,
-								  done=:done
-                				  WHERE todo_id=:todo_id`,
-		&oldTd)
+	result.Next()
+	var returned ToDo
+	err = result.Scan(&returned)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -150,7 +168,7 @@ func updateTodo(w http.ResponseWriter, r *http.Request) {
 	// Write response
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusAccepted)
-	marshalled, err := json.Marshal(oldTd)
+	marshalled, err := json.Marshal(returned)
 	w.Write([]byte(marshalled))
 }
 
@@ -169,16 +187,15 @@ func deleteTodo(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 	}
 
-	// Write response
-	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 }
 
 func StartServer() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", getTodos).Methods(http.MethodGet)
+	r.HandleFunc("/{todo_id}", getTodo).Methods(http.MethodGet)
 	r.HandleFunc("/", createTodo).Methods(http.MethodPost)
-	r.HandleFunc("/{todo_id}/", updateTodo).Methods(http.MethodPut)
-	r.HandleFunc("/{todo_id}/", deleteTodo).Methods(http.MethodDelete)
+	r.HandleFunc("/{todo_id}", updateTodo).Methods(http.MethodPut)
+	r.HandleFunc("/{todo_id}", deleteTodo).Methods(http.MethodDelete)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }

9a29c582b89e9e89d75e2c8e534cc4650fffff26
Danni Popova <danni.popova@unity3d.com>
2020-09-05 12:24:37 +0100

    chore: remove unused methods on todo service

--- internal/services/todo/interface.go
+++ internal/services/todo/interface.go
@@ -1,34 +1,22 @@
 package todo
 
 import (
-	"context"
 	"net/http"
 )
 
 type Service interface {
 	// Get the details for a single todanni identified by the given ID.
-	Get(context.Context, *GetRequest) (*Response, error)
-
-	// List all todannis.
-	List(context.Context, *ListRequest) (*ListResponse, error)
-
-	// Create a todanni.
-	Create(context.Context, *CreateRequest) (*Response, error)
-
-	// Update a todanni.
-	Update(context.Context, *UpdateRequest) (*Response, error)
-
-	// Delete a todanni identified by the given ID.
-	Delete(context.Context, *DeleteRequest) error
-
-	// Before I figure out how to refactor these:
 	GetHttp(w http.ResponseWriter, r *http.Request)
 
+	// List all todannis.
 	ListHttp(w http.ResponseWriter, r *http.Request)
 
+	// Create a todanni.
 	CreateHttp(w http.ResponseWriter, r *http.Request)
 
+	// Update a todanni.
 	UpdateHttp(w http.ResponseWriter, r *http.Request)
 
+	// Delete a todanni identified by the given ID.
 	DeleteHttp(w http.ResponseWriter, r *http.Request)
 }

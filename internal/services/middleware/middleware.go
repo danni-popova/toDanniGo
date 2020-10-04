f1b83874aadff1a25917b7af9b940aee5ca2d7dd
Danni Popova <danni.popova@unity3d.com>
2020-10-05 00:59:18 +0100

    holy shi* I made the context work

--- internal/services/middleware/middleware.go
+++ internal/services/middleware/middleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"context"
 	"net/http"
 	"strings"
 
@@ -19,6 +20,14 @@ import (
 
 type toDanniClaims struct {
 	jwt.StandardClaims
+
+	UserInfo userClaims `json:"user_info"`
+}
+
+type userClaims struct {
+	UserID         int    `json:"user_id"`
+	Email          string `json:"email"`
+	ProfilePicture string `json:"profile_picture"`
 }
 
 // Middleware function, which will be called for each request
@@ -28,11 +37,12 @@ func Middleware(next http.Handler) http.Handler {
 		splitToken := strings.Split(token, "Bearer ")
 		token = splitToken[1]
 
-		if validToken(token) {
+		if ctx, ok := validToken(token, r.Context()); ok {
 			// We found the token in our map
-			log.Printf("Authenticated token %s\n", token)
-			// Pass down the request to the next middleware (or final handler)
-			next.ServeHTTP(w, r)
+			log.Printf("Authenticated token %s", token)
+			// TODO: Instead of the whole token, just send the user ID
+			req := r.WithContext(ctx)
+			next.ServeHTTP(w, req)
 		} else {
 			// Write an error and stop the handler chain
 			http.Error(w, "Forbidden", http.StatusForbidden)
@@ -40,16 +50,18 @@ func Middleware(next http.Handler) http.Handler {
 	})
 }
 
-func validToken(tokenString string) bool {
-	token, err := jwt.ParseWithClaims(tokenString, &toDanniClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return []byte("the-todanni-secret"), nil
-	})
-
+func validToken(tokenString string, ctx context.Context) (context.Context, bool) {
+	token, err := jwt.ParseWithClaims(tokenString, &toDanniClaims{}, keyFunc)
 	if claims, ok := token.Claims.(*toDanniClaims); ok && token.Valid {
-		log.Info(claims.ExpiresAt)
-		return true
+		return context.WithValue(ctx, "user_id", claims.UserInfo.UserID), true
 	}
 
 	log.Error(err)
-	return false
+	return ctx, false
+}
+
+func keyFunc(token *jwt.Token) (interface{}, error) {
+	// TODO: later the "kid" can be used to check the version of the key used to sign the JWT
+	// This will come in handy when key rotation is implemented.
+	return []byte("the-todanni-secret"), nil
 }
